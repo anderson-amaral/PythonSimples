4a4d1f5c738a381eb42797fd9678a0be2735d787
Ander <ander.roque2012@gmail.com>
2025-11-21 19:27:59 -0300

    Envio do Projeto

---
+++ .vscode/exemplo.py
@@ -0,0 +1,8 @@
+print("Testando arquivo de programa .py")
+nome=input("Qual é seu nome:")
+a = input("Primeiro número: ")
+b = input("Segundo número: ")
+
+r = int(a) * int(b)
+
+print(f"{a} + {b} = {r}")
